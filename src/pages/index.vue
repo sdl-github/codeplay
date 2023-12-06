f9bb2296b009a55a4434151e84d0bf03a5c3a4bf
sdl-code <sdl-life@foxmail.com>
2023-12-06 10:54:35 +0800

    CodePlayer

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,22 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+<script setup lang="ts">
+import type { CodePlayerOptions } from 'codeplayer'
+import CodePlayer from 'codeplayer'
+import 'codeplayer/dist/style.css'
 
-const name = ref('')
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+const options: CodePlayerOptions = {
+  appType: 'vue3',
 }
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
+    <CodePlayer :options="options" class="codeplayer-container" />
   </div>
 </template>
+
+<style scoped>
+  .codeplayer-container {
+    max-height: 100vh;
+    height: 100vh;
+  }
+</style>
